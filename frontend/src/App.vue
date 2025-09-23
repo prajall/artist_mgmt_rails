1ba556e63d6ea665c81d75ef498c85c4e270838e
Prajal Maharjan <prajalmhrzn@gmail.com>
2025-09-23 16:47:28 +0545

    Intialize frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+</template>
+
+<style scoped></style>
